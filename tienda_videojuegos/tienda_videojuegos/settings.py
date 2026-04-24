0dd54171e7a547e4b4967dc2fc4c8fb1e57c995d
César <cesarjosedugartebrizuela@gmail.com>
2026-04-24 15:45:57 -0400

    feat: agregar aplicación 'catalogo' con archivos iniciales y configuración

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalogo',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'America\Caracas'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
